427316e4c6b4a0f846dab26edd783c42f84ef08e
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-02-26 19:42:17 +0100

    add grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Books from "./books/page";
 
 export default function Home() {
   return (
-    <main>
+    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center py-12 px-4">
       <Books />
     </main>
   );
