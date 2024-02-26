ac8bf814475ebe1932a7710e74ef36e0609a25d5
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-02-26 23:01:39 +0100

    created

---
+++ src/components/layout/AddBookButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export function AddBookButton() {
+  return (
+    <Button
+      variant="destructive"
+      size="lg"
+      className="bg-gradient-to-r from-emerald-500 to-emerald-700 my-5 uppercase font-bold text-white hover:brightness-110 "
+    >
+      Dodaj
+    </Button>
+  );
+}
