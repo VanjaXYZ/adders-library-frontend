9db4d8545b768d2740948344c926ae2fb0954634
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-02-26 23:01:08 +0100

    add modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { CreateBookModal } from "@/components/layout/CreateBookModal";
 import Books from "./books/page";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center py-12 px-4">
-      <Books />
+    <main>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center place-items-center pb-12 pt-4 px-4">
+        <CreateBookModal />
+        <Books />
+      </section>
     </main>
   );
 }
