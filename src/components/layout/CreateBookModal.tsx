7fe7c7495212a8747ce6bfd222220b2b4120f890
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-02-27 02:42:06 +0100

    extracted Form el. as separate comp.

--- src/components/layout/CreateBookModal.tsx
+++ src/components/layout/CreateBookModal.tsx
@@ -1,38 +1,16 @@
-"use client";
+// "use client";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "../ui/textarea";
-import { useState } from "react";
-import { createNewBook } from "@/api/apiMethods";
-import { Book } from "@/app/books/interfaces";
+import Form from "./Form";
 
 export function CreateBookModal() {
-  const [book, setBook] = useState<Book>({
-    title: "",
-    description: "",
-    author: "",
-  });
-
-  const onCreateNewBook = async (book: any) => {
-    try {
-      const createBook = await createNewBook(book);
-      if (createBook?.status === 200) {
-        return createBook;
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -48,59 +26,7 @@ export function CreateBookModal() {
           <DialogTitle>Dodaj knjigu</DialogTitle>
           <DialogDescription>Unesi podatke za novu knjigu.</DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="title" className="text-right">
-              Naslov
-            </Label>
-            <Input
-              id="title"
-              maxLength={60}
-              placeholder="Max. 60 karaktera"
-              className="col-span-3"
-              onChange={(e) => setBook({ ...book, title: e.target.value })}
-              required
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="author" className="text-right">
-              Autor
-            </Label>
-            <Input
-              id="author"
-              maxLength={30}
-              placeholder="Max. 30 karaktera"
-              className="col-span-3"
-              onChange={(e) => setBook({ ...book, author: e.target.value })}
-              required
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="description" className="text-right self-start">
-              Opis
-            </Label>
-            <Textarea
-              id="description"
-              maxLength={100}
-              placeholder="Max. 100 karaktera"
-              className="col-span-3 resize-none"
-              onChange={(e) =>
-                setBook({ ...book, description: e.target.value })
-              }
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <DialogTrigger asChild>
-            <Button
-              type="submit"
-              className="bg-emerald-500 hover:brightness-90 text-white"
-              onClick={() => onCreateNewBook(book)}
-            >
-              Sačuvaj
-            </Button>
-          </DialogTrigger>
-        </DialogFooter>
+        <Form />
       </DialogContent>
     </Dialog>
   );
