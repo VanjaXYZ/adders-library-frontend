f5b2cea52f7ae43d6bfb1e134719cbc04e9b39e1
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-02-27 02:41:25 +0100

    created

---
+++ src/app/books/Books.tsx
@@ -0,0 +1,19 @@
+import { getBooksCollection } from "@/api/apiMethods";
+import BookCard from "./components/BookCard";
+import { Book } from "./interfaces";
+
+const Books = async () => {
+  const books = await getBooksCollection();
+  return books?.map((book: Book) => (
+    <div className="w-full" key={book._id}>
+      <BookCard
+        title={book.title}
+        id={book._id}
+        author={book.author}
+        description={book.description}
+      />
+    </div>
+  ));
+};
+
+export default Books;
