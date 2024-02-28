d31c5b6e44bfef7f80e0d6157e71fc0c5748b2ef
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-02-28 23:42:14 +0100

    add delete method

--- src/api/apiMethods.ts
+++ src/api/apiMethods.ts
@@ -5,6 +5,7 @@ import { Config } from "../../Config";
 import { revalidatePath } from "next/cache";
 
 // Get books collection
+
 export const getBooksCollection = async () => {
   try {
     const response = await axios.get(`${Config.baseURL}/books`);
@@ -21,14 +22,31 @@ export const getBooksCollection = async () => {
 // Create new book
 
 export const addBook = async (formData: FormData) => {
-  const bookTitle = formData.get("title");
-  const bookAuthor = formData.get("author");
-  const bookDescription = formData.get("description");
-  const createBook = await axios.post(`${Config.baseURL}/books`, {
-    title: bookTitle,
-    author: bookAuthor,
-    description: bookDescription,
-  });
-  revalidatePath("/");
-  return createBook;
+  try {
+    const bookTitle = formData.get("title");
+    const bookAuthor = formData.get("author");
+    const bookDescription = formData.get("description");
+    const createBook = await axios.post(`${Config.baseURL}/books`, {
+      title: bookTitle,
+      author: bookAuthor,
+      description: bookDescription,
+    });
+    revalidatePath("/");
+    return createBook;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Delete book
+
+export const deleteBook = async (formData: FormData) => {
+  try {
+    const bookID = formData.get("id");
+    const removeBook = await axios.delete(`${Config.baseURL}/books/${bookID}`);
+    revalidatePath("/");
+    return removeBook;
+  } catch (error) {
+    console.error(error);
+  }
 };
