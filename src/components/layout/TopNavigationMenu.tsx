c040c298326e0585047f2ad3b3acf21ae26b0eaf
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-02 05:49:13 +0100

    add logo-text-image

--- src/components/layout/TopNavigationMenu.tsx
+++ src/components/layout/TopNavigationMenu.tsx
@@ -15,6 +15,7 @@ import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import ADDER_LOGO from "@/app/assets/adder-logo.png";
+import ADDER_TITLE from "@/app/assets/title-logo.png";
 
 const TopNavigationMenu = () => {
   const pathname = usePathname();
@@ -23,9 +24,10 @@ const TopNavigationMenu = () => {
       <div>
         <Image src={ADDER_LOGO} alt="adder_logo" width={25} height={25} />
       </div>
-      <h3 className="text-white uppercase text-2xl tracking-widest hidden sm:flex">
-        Adder's Library
-      </h3>
+
+      <div className="hidden sm:flex ms-36">
+        <Image src={ADDER_TITLE} alt="adder_logo" width={225} height={25} />
+      </div>
       <NavigationMenu>
         <NavigationMenuList className={cn("flex gap-10")}>
           <NavigationMenuLink
