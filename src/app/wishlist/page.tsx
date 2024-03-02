a4b0836aa7998371b5b28064161dac6a264d407a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-02 05:27:54 +0100

    add dummy layout

---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Wishlist = () => {
+  return (
+    <div className="text-white px-14">
+      <h1 className="my-10 text-2xl">FRAME TRANSITIONS</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro vero iusto
+      consequuntur, quibusdam eligendi, itaque quas autem dicta temporibus
+      maiores debitis fuga? Maiores, doloremque asperiores sint qui molestias
+      deserunt expedita natus rerum libero odit rem aut facere? Quo fuga
+      laudantium impedit magni maxime quae iure modi similique eos architecto
+      cupiditate, illo, consequatur soluta dignissimos error corporis ab.
+      Asperiores repellendus culpa expedita accusantium sint, in dolores? Neque
+      earum unde maxime eum beatae voluptas quia, sapiente ipsam iste illum
+      exercitationem tempore fuga pariatur magnam quidem impedit, dolores in eos
+      ullam nemo eveniet. A, ut distinctio alias et beatae impedit placeat sed
+      corrupti!
+    </div>
+  );
+};
+
+export default Wishlist;
