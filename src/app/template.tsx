fb59ab6d3435876d4b8e52b14148ec5fb2d19c0d
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-04 23:39:16 +0100

    change opacity

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const Template = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.div
-      initial={{ y: 20, opacity: 0 }}
+      initial={{ y: 20, opacity: 0.5 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ ease: "easeIn", duration: 0.15 }}
     >
