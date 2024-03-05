4cfa7dedf90324346cba7183c88ebb893beed854
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-06 00:01:19 +0100

    add cookies

--- src/components/layout/RightNavigationMenu.tsx
+++ src/components/layout/RightNavigationMenu.tsx
@@ -1,32 +1,29 @@
 "use client";
-import React from "react";
+import ADDER_TITLE from "@/app/assets/title-logo.png";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Sheet,
-  SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { CiMenuFries } from "react-icons/ci";
-import ADDER_TITLE from "@/app/assets/title-logo.png";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { IconContext } from "react-icons";
+import { CiMenuFries } from "react-icons/ci";
 import {
   NavigationMenu,
   NavigationMenuLink,
   NavigationMenuList,
 } from "../ui/navigation-menu";
-import { cn } from "@/lib/utils";
-import { usePathname } from "next/navigation";
-import { IconContext } from "react-icons";
+import LogoutButton from "./LogoutButton";
+import Cookies from "js-cookie";
 
 const RightNavigationMenu = () => {
   const pathname = usePathname();
+  const token = Cookies.get("token");
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -47,61 +44,45 @@ const RightNavigationMenu = () => {
               priority={true}
             />
           </SheetTitle>
-          {/* <SheetDescription>
-            Make changes to your profile here. Click save when you're done.
-          </SheetDescription> */}
         </SheetHeader>
-        {/* <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Adder" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit" variant={"outline"}>
-              Save changes
-            </Button>
-          </SheetClose>
-        </SheetFooter> */}
-        <div className="w-full">
-          <NavigationMenu className="w-full m-auto mt-10">
-            <NavigationMenuList
-              className={cn(
-                "flex flex-col justify-center items-center w-full m-auto gap-10"
-              )}
-            >
-              <NavigationMenuLink
-                className={cn(
-                  `hover:text-white text-black text-2xl ${
-                    pathname === "/" ? "text-white " : ""
-                  }`
-                )}
-                href="/"
-              >
-                Library
-              </NavigationMenuLink>
-              <NavigationMenuLink
-                className={cn(
-                  `hover:text-white text-black text-2xl ${
-                    pathname === "/wishlist" ? "text-white" : ""
-                  }`
-                )}
-                href="/wishlist"
-              >
-                Wishlist
-              </NavigationMenuLink>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
+
+        {token ? (
+          <>
+            <div className="w-full">
+              <NavigationMenu className="w-full m-auto mt-10">
+                <NavigationMenuList
+                  className={cn(
+                    "flex flex-col justify-center items-center w-full m-auto gap-10"
+                  )}
+                >
+                  <NavigationMenuLink
+                    className={cn(
+                      `hover:text-white text-black text-2xl ${
+                        pathname === "/" ? "text-white " : ""
+                      }`
+                    )}
+                    href="/"
+                  >
+                    Library
+                  </NavigationMenuLink>
+                  <NavigationMenuLink
+                    className={cn(
+                      `hover:text-white text-black text-2xl ${
+                        pathname === "/wishlist" ? "text-white" : ""
+                      }`
+                    )}
+                    href="/wishlist"
+                  >
+                    Wishlist
+                  </NavigationMenuLink>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </div>
+            <div className="text-center mt-8">
+              <LogoutButton />
+            </div>
+          </>
+        ) : null}
       </SheetContent>
     </Sheet>
   );
