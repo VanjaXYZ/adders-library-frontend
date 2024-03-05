45b10eb8b635a4d84b94ad4c7d7eb7a428ede3e2
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-05 19:39:48 +0100

    add toast mssg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TopNavigationMenu from "@/components/layout/TopNavigationMenu";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           <TopNavigationMenu />
         </div>
         {children}
+        <Toaster />
       </body>
     </html>
   );
