11bf83bb9377fc1a66f33d3dffe8e14c1e364d78
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-07 10:57:01 +0100

    created portecte_routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,14 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
-  if (!token && request.nextUrl.pathname !== "/login") {
+
+  // If there is no token and the user is trying to access pages other than /login or /register, redirect to /login
+  if (!token && !["/login", "/register"].includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-  if (token && request.nextUrl.pathname === "/login") {
+
+  // If the user is already logged in and tries to access /login or /register, redirect to the home page
+  if (token && ["/login", "/register"].includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
