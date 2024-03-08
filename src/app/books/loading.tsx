7a68b65e943ec1951dc699c55ebadc8b617dfb99
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-03-08 22:12:51 +0100

    created main loading state

---
+++ src/app/books/loading.tsx
@@ -0,0 +1,12 @@
+import MainLoading from "@/components/loadings/MainLoading";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <>
+      <MainLoading />
+    </>
+  );
+};
+
+export default Loading;
